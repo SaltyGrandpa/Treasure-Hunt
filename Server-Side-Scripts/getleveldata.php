d58e0bfd37c47272f454d6f6d6db97ad0c5e7ccf
Corey McCown <coreymccown13@gmail.com>
2016-01-10 13:42:31 -0500

    Create getleveldata.php
    Used for fetching serialized level data to be downloaded and parsed within Unity3D.

---
+++ Server-Side-Scripts/getleveldata.php
@@ -0,0 +1,41 @@
+<?php 
+	//Only execute script if all variables are set
+	if(!(isset($_POST["id"])) ||
+	(($_POST["id"] == ""))
+	)
+		die("ABORTED: Not enough information to get level data");
+	else {
+		
+		
+		try {
+		/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
+		/*  Selection of points within specified radius of given lat/lon      (c) Chris Veness 2008-2014  */
+		/*  Originally found at: http://www.movable-type.co.uk/scripts/latlong-db.html                    */
+		/*  Modifications to use milage and adapt to database design by Corey McCown                      */
+		/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
+			//Begin database connection
+			//Connect using my shared credentials file
+			//Database hosted locally, database name is 'SeniorProject'
+			include_once("../includes/sql.inc.php");
+			$db = new PDO("mysql:host=localhost;dbname=SeniorProject;", DB_USER, DB_PASSWORD);
+			$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+		
+			$id = $_POST["id"]; // radius of bounding circle in miles
+			
+			$sql = "SELECT data FROM augmented WHERE id = :id";
+			$params = array(
+				'id' => $id,
+			);
+			$points = $db->prepare($sql);
+			$points->execute($params);
+			$points->setFetchMode(PDO::FETCH_ASSOC);
+			while($row = $points->fetch()) {
+				echo $row['data'];	
+			}
+			
+		//Display error if the post is unsuccessful
+		} catch (Exception $e) {
+			die("An error has occurred while submitting the level data: " . $e);
+		}
+	}
+?>
